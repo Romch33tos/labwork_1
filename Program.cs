7ef436c4cab2cf4f9b1114a1dd97ae663404a550
Romch33tos <Romch33tos@yandex.ru>
2025-02-11 21:30:49 +0700

    Функция возведения в степень

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
-﻿﻿using System;
+﻿using System;
 class Program
 {
     static void Main()
     {
       Console.Write("Введите число: ");
       int number = Convert.ToInt32(Console.ReadLine());
+
       Console.Write("Введите показатель степени: ");
       int degree = Convert.ToInt32(Console.ReadLine());
-      Console.WriteLine($"{number} {degree}");
+
+      double result = Math.Round(Math.Exp(degree * Math.Log(number)));
+
+      Console.WriteLine($"{number}^{degree} = {result}");
     }
 }
