527c6f98afa36a60539eb64280c9eb14db6eacff
Romch33tos <Romch33tos@yandex.ru>
2025-02-11 21:27:11 +0700

    Ввод и вывод

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿﻿using System;
+class Program
+{
+    static void Main()
+    {
+      Console.Write("Введите число: ");
+      int number = Convert.ToInt32(Console.ReadLine());
+      Console.Write("Введите показатель степени: ");
+      int degree = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine($"{number} {degree}");
+    }
+}
