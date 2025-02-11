e7fd580533a561521fc9634f7ab349e9936951b3
Romch33tos <Romch33tos@yandex.ru>
2025-02-11 21:36:53 +0700

    Второе задание

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-﻿﻿using System;
+﻿using System;
 
 class Program
 {
-   static void firstProgram() {
+   static void firstProgram() 
+   {
       Console.Write("Введите число: ");
       int number = Convert.ToInt32(Console.ReadLine());
 
@@ -11,20 +12,28 @@ class Program
 
       double result = Math.Round(Math.Exp(degree * Math.Log(number)));
 
-      Console.WriteLine($"{number}^{degree} = {result}");
+      Console.WriteLine($"{number}^{degree} = {result}\n");
     }
 
-    static void secondProgram() {
-      Console.Write("Программа выполнена");
+    static void secondProgram() 
+    {
+      Console.Write("Введите число x: ");
+      string x_number = Console.ReadLine();
+
+      char secondDigit = x_number[1];
+      string newNumber = x_number.Remove(1, 1) + secondDigit;
+
+      Console.WriteLine($"Число n: {newNumber}\n");
     }
 
     static void Main()
     {
       string choice;
-      while (true) {
-        Console.WriteLine(
-          "описание функций");
-          choice = Console.ReadLine();
+
+      while (true) 
+      {       
+        Console.WriteLine("Введите номер команды:\n1. Запуск программы №1\n2. Запуск программы №2\n3. Выход");
+        choice = Console.ReadLine();
 
           switch(choice) {
             case "1":
@@ -33,7 +42,11 @@ class Program
             case "2":
               secondProgram();
               break;
+            case "3":
+              Environment.Exit(0);
+              break;
             default:
+              Console.WriteLine("Некорректная команда!");
               break;
           }     
       }
